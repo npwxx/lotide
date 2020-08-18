5dcfde17f3c1b7643f7a184a83c19f8c737c4764
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-18 03:21:14 +0000

    Implemented assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🙅‍♀️🤦‍♀️‍Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`👌👌Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hahaha", "hahaha");
+assertEqual(1, 2);
