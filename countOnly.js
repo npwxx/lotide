43701251fd05bd771adde9421a75dbe1114b9243
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-19 20:39:48 +0000

    Implemented countOnly.js assignment

---
+++ countOnly.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🙅‍♀️🤦‍♀️‍Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`👌👌Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    console.log(item);
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
