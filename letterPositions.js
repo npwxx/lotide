d099e1ca93231066ce31b0f57c0cf9592b1eef4b
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-19 22:13:40 +0000

    Added tests

--- letterPositions.js
+++ letterPositions.js
@@ -16,5 +16,34 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-thisString = letterPositions("some sentence");
-console.log(thisString);
+// thisString = letterPositions("some sentence");
+// console.log(thisString);
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1 === undefined && arr2 === undefined) {
+    return true;
+  }
+  if (arr1 === undefined || arr2 === undefined) {
+    return false;
+  }
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👌👌Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙅‍♀️🤦‍♀️‍Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
