b24722db73bf79e0ede47d041ec51f9f8e9f2538
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-19 21:32:46 +0000

    added extra conditionals

--- eqArrays.js
+++ eqArrays.js
@@ -9,6 +9,12 @@ const assertEqual = function(actual, expected) {
 // Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 
 const eqArrays = function(arr1, arr2) {
+  if (arr1 === undefined && arr2 === undefined) {
+    return true;
+  }
+  if (arr1 === undefined || arr2 === undefined) {
+    return false;
+  }
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
