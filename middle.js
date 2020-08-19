a1068460f7f1c5b49dee7dbb8e60ba8cf11d2079
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-19 19:24:13 +0000

    Implemented middle.js assignment

---
+++ middle.js
@@ -0,0 +1,47 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👌👌Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙅‍♀️🤦‍♀️‍Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1 === undefined && arr2 === undefined) {
+    return true;
+  }
+  if (arr1 === undefined || arr2 === undefined) {
+    return false;
+  }
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const middle = function(arrInput) {
+  if (arrInput.length <= 2) {
+    return [];
+  }
+  if (arrInput.length % 2 !== 0) {
+    let oddMiddleIndex = Math.floor(arrInput.length / 2);
+    return [arrInput[oddMiddleIndex]];
+  }
+  if (arrInput.length % 2 === 0) {
+    let evenMiddleIndex = arrInput.length / 2;
+    return [arrInput[evenMiddleIndex -1], arrInput[evenMiddleIndex]];
+  }
+
+}
+assertArraysEqual(middle([1]), []) // => []
+assertArraysEqual(middle([1, 2]), []) // => []
+assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
