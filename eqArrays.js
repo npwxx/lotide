405f01f550c57f52f00598c6b8fc0cd8f3bec590
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-19 03:33:53 +0000

    Implemented eqArrays assignment

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🙅‍♀️🤦‍♀️‍Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`👌👌Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+// Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 3, 2]), false);
+assertEqual(eqArrays(["1", 2, 3], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays([], []), true);
