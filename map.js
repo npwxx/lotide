7cb3466bb06fae31cb032c6a3778e419c608f4ba
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-21 01:37:25 +0000

    Implement Map assignment

---
+++ map.js
@@ -0,0 +1,43 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👌👌Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙅‍♀️🤦‍♀️‍Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1 === undefined && arr2 === undefined) {
+    return true;
+  }
+  if (arr1 === undefined || arr2 === undefined) {
+    return false;
+  }
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+const results2 = map(words, word => word);
+console.log(results2);
+const results3 = map(words, word => word.split("").reverse().join(""));
+console.log(results3);
