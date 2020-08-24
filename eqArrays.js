9cbc7f5696c988f7a2dad98d6a8a8f8be448ae18
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-24 15:28:01 +0000

    Refactored EqArrays and added separate test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,4 @@
-const assertEqual = require("./assertEqual");
-/*const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🙅‍♀️🤦‍♀️‍Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`👌👌Assertion Passed: ${actual} === ${expected}`);
-  }
-};*/
-
 // Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
-
 const eqArrays = function(arr1, arr2) {
   if (arr1 === undefined && arr2 === undefined) {
     return true;
@@ -27,13 +17,5 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
 };
-
-
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-//assertEqual(eqArrays([1, 2, 3], [1, 3, 2]), false);
-//assertEqual(eqArrays(["1", 2, 3], [1, 2, 3]), false);
-//assertEqual(eqArrays([1, 2, 3], [1]), false);
-//assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-//assertEqual(eqArrays([], []), true);
 
 module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,8 @@
+const eqArrays = require("../eqArrays");
+const assertEqual = require("../assertEqual");
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 3, 2]), false);
+assertEqual(eqArrays(["1", 2, 3], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays([], []), true);
