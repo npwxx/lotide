453198a09c26e64802112c41ee0bd711fbd6b64d
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-24 02:03:28 +0000

    implemented flatten function

--- flatten.js
+++ flatten.js
@@ -1,25 +1,24 @@
 const assertArraysEqual = require("./assertArraysEqual");
-const eqArrays = require("./eqArrays");
+//const eqArrays = require("./eqArrays");
 
-const flatten = function () {
-
+const arr = [1,2,[3,4],5,[6]];
+const flatten = function(arr) {
+  let flattenedArr = [];
+  for (const elem of arr) {
+    if (!Array.isArray(elem)) {
+      flattenedArr.push(elem);
+      //console.log(flattenedArr);
+    } else {
+      for (const elems of elem) {
+        flattenedArr.push(elems);
+        //console.log(flattenedArr);
+      }
+    }
+  }
+  //console.log(flattenedArr);
+  return flattenedArr;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(flatten(arr));
+assertArraysEqual(flatten(arr), [1,2,3,4,5,6]);
 
 module.exports = flatten;
